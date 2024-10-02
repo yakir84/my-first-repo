28f4c79e0b4ded1909892f41790a6cc7a4f44854
Yakir zuberi <yakir7784@gmail.com>
2024-10-02 16:15:20 +0300

    Adding another student row

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("yakir", 40, "English", 4 ,45)
+student = Student("Alex", 30, "English", 3 ,85)
+student_2 = Student("yakir", 40, "English", 4 ,98)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
